ff3a35588ba3ae8e7b9377745b5630002c9a9258
RicardoFernandez <rreyesfdz96@gmail.com>
2023-01-05 11:43:58 -0600

    Prb Heroku

--- src/db.js
+++ src/db.js
@@ -3,13 +3,20 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+  // logging: false, // set to console.log to see the raw SQL queries
+  // native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+console.log(sequelize);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];

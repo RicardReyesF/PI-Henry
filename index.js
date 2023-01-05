363bae92868d4952ae337a2ec9badaaf7b84383f
RicardoFernandez <rreyesfdz96@gmail.com>
2023-01-05 13:04:07 -0600

    Cambios en el puerto

--- index.js
+++ index.js
@@ -20,9 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const PORT = process.env.PORT || 5000
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
